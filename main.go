cf8430e2c15534857840dd9b50bffc90044d33f7
ftb2024_official <ftb2024@gmail.com>
2024-06-06 18:46:41 +0500

    create contact implemented

--- main.go
+++ main.go
@@ -6,37 +6,92 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// type Templates struct {
-// 	templates *template.Template
-// }
+type Contact struct {
+	Name  string
+	Email string
+}
 
-// func (t *Templates) Render(wr io.Writer, name string, data interface{}, ctx *gin.Context) error {
-// 	return t.templates.ExecuteTemplate(wr, name, data)
-// }
+func newContact(name, email string) Contact {
+	return Contact{Name: name, Email: email}
+}
 
-// func newTemplate() *Templates {
-// 	return &Templates{
-// 		templates: template.Must(template.ParseGlob("views/*.html")),
-// 	}
-// }
+type Contacts []Contact
 
-type Count struct {
-	Count int
+type Data struct {
+	Contacts Contacts
+}
+
+func (d *Data) hasEmail(email string) bool {
+	for _, c := range d.Contacts {
+		if c.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
+func newData() Data {
+	return Data{
+		Contacts: []Contact{
+			newContact("John", "john@gmail.com"),
+			newContact("Adam", "adam@gmail.com"),
+		},
+	}
+}
+
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func newFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Page struct {
+	Data     Data
+	FormData FormData
+}
+
+func newPage() Page {
+	return Page{
+		Data:     newData(),
+		FormData: newFormData(),
+	}
 }
 
 func main() {
 	router := gin.Default()             // создаем роутер Gin
 	router.LoadHTMLGlob("views/*.html") // указываем, где искать HTML файлы
 
-	count := Count{Count: 0}
+	page := newPage()
 
 	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(http.StatusOK, "index", count)
+		ctx.HTML(http.StatusOK, "index", page)
 	})
 
-	router.POST("/count", func(ctx *gin.Context) {
-		count.Count++
-		ctx.HTML(http.StatusOK, "count", count)
+	router.POST("/contacts", func(ctx *gin.Context) {
+		name := ctx.PostForm("name")
+		email := ctx.PostForm("email")
+
+		if page.Data.hasEmail(email) {
+			formData := newFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+			formData.Errors["email"] = "Email already exists"
+
+			ctx.HTML(422, "form", formData)
+			return
+		}
+
+		contact := newContact(name, email)
+		page.Data.Contacts = append(page.Data.Contacts, contact)
+
+		ctx.HTML(http.StatusOK, "form", newFormData())
+		ctx.HTML(http.StatusOK, "oob-contact", contact)
 	})
 
 	_ = router.Run(":8080") // запускаем сервер на порту 8080
